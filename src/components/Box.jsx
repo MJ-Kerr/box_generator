da1004144951e994a8f4b93515b53673ce1926a0
Miles Kerr <m.kerr.professional@gmail.com>
2023-05-08 23:09:58 -0500

    actual commit

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
-const Display = (props) => {
+// this assigns the obj div
+const Box = (props) => {
+    // console.log(props)
     return (
-        <fieldset>
-            <legend>Display.jsx</legend>
-            
-        </fieldset>
+        // returns style for the obj div
+        <div style={{ backgroundColor: props.boxObj.color, width: "100px", height: "100px", display: "inline-block", margin: "5px" }}>
+            {/* this is the actual return of the obj */}
+            {props.boxObj.color}
+        </div>
     )
 }
 
-export default Display
+export default Box 
